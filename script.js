68e0e2d02eb22a3ff528779bc16a6e60047ec75b
user.name <S10267504@connect.np.edu.sg>
2025-01-24 01:45:15 +0800

    Updated navbar

---
+++ script.js
@@ -0,0 +1,4 @@
+window.onscroll = function() {
+    let scrolled = (document.documentElement.scrollTop || document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
+    document.getElementById('scroll-progress').style.width = scrolled + '%';
+}
