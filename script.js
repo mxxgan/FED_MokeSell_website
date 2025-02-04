ee81c410258d020d129ddbf0a11d116f4b6de052
user.name <S10267504@connect.np.edu.sg>
2025-02-04 10:58:37 +0800

    litsing page

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ window.onscroll = function() {
     let scrolled = (document.documentElement.scrollTop || document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
     document.getElementById('scroll-progress').style.width = scrolled + '%';
 }
+
+
